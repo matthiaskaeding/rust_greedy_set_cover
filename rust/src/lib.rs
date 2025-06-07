9888a9074ee9f38ca52f99d0ab193901c1334a7c
matthiaskaeding <kaedingmatthias@gmail.com>
2025-06-07 16:23:31 +0200

    Make testing work by extension module

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,15 +1,24 @@
+// Add the cfg attribute to the pyo3 imports
+#[cfg(feature = "extension-module")]
 use pyo3::prelude::*;
+#[cfg(feature = "extension-module")]
 use pyo3::types::PyModule;
-use std::collections::{HashMap, HashSet}; // Make sure this use statement is present
 
+use std::collections::{HashMap, HashSet};
+
+// This module contains your core logic and tests, it should always be included.
 mod greedy_set_cover;
 
+// Add the cfg attribute to the pymodule block
+#[cfg(feature = "extension-module")]
 #[pymodule]
 fn setcover_lib(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(greedy_set_cover_py, m)?)?;
     Ok(())
 }
 
+// Add the cfg attribute to the pyfunction wrapper
+#[cfg(feature = "extension-module")]
 #[pyfunction]
 fn greedy_set_cover_py(sets: HashMap<String, Vec<i32>>, algo: i16) -> PyResult<HashSet<String>> {
     Ok(greedy_set_cover::greedy_set_cover(&sets, algo))
