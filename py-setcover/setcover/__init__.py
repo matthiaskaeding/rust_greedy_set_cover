3cd6ecf9d5c753de1cfc588d5ca34ddbc2c6a2d0
matthiaskaeding <kaedingmatthias@gmail.com>
2025-06-07 22:40:25 +0200

    Clean init

--- py-setcover/setcover/__init__.py
+++ py-setcover/setcover/__init__.py
@@ -27,12 +27,6 @@ def setcover(sets: dict, algo: str = "greedy-1") -> set:
             raise ValueError(msg)
 
 
-def test(x):
-    return x
-
-
 __all__ = [
     "setcover",
-    test,
-    # "my_python_helper" # <- Add other public functions here
 ]
