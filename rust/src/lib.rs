d89e7e19502bf69f2176b2733b41fca2203d5be4
matthiaskaeding <kaedingmatthias@gmail.com>
2025-06-07 13:03:34 +0200

    pyo3

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,1 +1,15 @@
-pub mod greedy_set_cover;
+use pyo3::prelude::*;
+use std::collections::{HashMap, HashSet};
+
+mod greedy_set_cover;
+
+#[pymodule]
+fn set_cover(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(greedy_set_cover_py, m)?)?;
+    Ok(())
+}
+
+#[pyfunction]
+fn greedy_set_cover_py(sets: HashMap<String, Vec<i32>>, algo: i16) -> PyResult<HashSet<String>> {
+    Ok(greedy_set_cover::greedy_set_cover(&sets, algo))
+}
